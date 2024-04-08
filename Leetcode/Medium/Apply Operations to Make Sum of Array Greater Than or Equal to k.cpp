00bee8da5d9f9e215850a95685541eb1066c85c3
Santosh Kumar <51458614+Ssantosh12@users.noreply.github.com>
2024-04-08 09:49:55 +0530

    Update Apply Operations to Make Sum of Array Greater Than or Equal to k.cpp

--- Leetcode/Medium/Apply Operations to Make Sum of Array Greater Than or Equal to k.cpp
+++ Leetcode/Medium/Apply Operations to Make Sum of Array Greater Than or Equal to k.cpp
@@ -3,6 +3,7 @@ public:
     int minOperations(int k) {
         // initially I thought this solution won't work and this is some lengthy type of problem
         // but this is correct so MAJOR LEARNING : Always Trust your intuition and never be afraid
+        
         int num,diff,count=0,mnCount=k-1;
         for(num=1;num<=k;num++){
             count=num-1;
